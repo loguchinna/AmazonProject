0d42ca9301400d09923d74a709ea4470864edc1d
loguchinna <loganathan7959@gmail.com>
2022-06-07 10:42:20 +0530

    Modify code

--- src/test/java/org/project/IphoneList.java
+++ src/test/java/org/project/IphoneList.java
@@ -1,12 +1,15 @@
 package org.project;
 
+import org.test.home.BaseClass;
 import org.testng.annotations.Test;
 
-public class IphoneList {
+public class IphoneList extends BaseClass {
 
 	@Test
 	private void test() 
 	{
 		System.out.println("Iphone List......");
+		urlLaunch("https://www.amazon.in/");
+		
 	}
 }
